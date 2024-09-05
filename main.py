88ada74978c9b9f12805a62bec2d3755838cd6a6
Jonatan Cervall <jonatancervall@gmail.com>
2024-09-05 15:25:42 +0200

    Klar för användning!

--- main.py
+++ main.py
@@ -18,18 +18,24 @@ STAR_HEIGHT = 20
 STAR_VEL = 3
 
 FONT = pygame.font.SysFont("comicsans", 30)
+orgimage = pygame.image.load('girl.jpg')
+image = pygame.transform.scale(orgimage,(40,60))
 
+morgimage = pygame.image.load('man.png')
+mimage = pygame.transform.scale(morgimage,(20,30))
 
 def draw(player, elapsed_time, stars):
+    pygame.Surface.blit(image, WIN, player)
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
-    pygame.draw.rect(WIN, "red", player)
+    WIN.blit(image, player)
+    #pygame.draw.rect(WIN, "red", player)
 
     for star in stars:
-        pygame.draw.rect(WIN, "white", star)
+        WIN.blit(mimage, star)
 
     pygame.display.update()
 
@@ -37,8 +43,7 @@ def draw(player, elapsed_time, stars):
 def main():
     run = True
 
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,
-                         PLAYER_WIDTH, PLAYER_HEIGHT)
+    player = WIN.blit(image,(200,HEIGHT - PLAYER_HEIGHT))
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
@@ -48,6 +53,15 @@ def main():
 
     stars = []
     hit = False
+    start_text = FONT.render("Akta dig för Dansgalningar! Tryck högerpil för start", 1, "white")
+    WIN.blit(start_text, (WIDTH/2 - start_text.get_width()/2, HEIGHT/2 - start_text.get_height()/2))
+    pygame.display.update()
+    while True:
+        e = pygame.event.wait()
+        if e.type == pygame.QUIT:
+            pygame.quit()
+        elif e.type == pygame.KEYDOWN:
+            break
 
     while run:
         star_count += clock.tick(60)
@@ -84,11 +98,16 @@ def main():
                 break
 
         if hit:
-            lost_text = FONT.render("You Lost!", 1, "white")
+            lost_text = FONT.render("Vill du boka danskurs med mig?", 1, "white")
             WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
             pygame.display.update()
-            pygame.time.delay(4000)
-            break
+            while True:
+                e = pygame.event.wait()
+                if e.type == pygame.QUIT:
+                    pygame.quit()
+                elif e.type == pygame.KEYDOWN:
+                    break
+            
 
         draw(player, elapsed_time, stars)
 
